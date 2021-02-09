e02a40507791a924ea0123d20e59de0537c31139
Leonardo Giraldi <leogiraldimg@gmail.com>
2021-02-09 16:59:04 -0300

    Conteúdo até a última aula

--- SingleSignOn/test_my_service.py
+++ SingleSignOn/test_my_service.py
@@ -15,3 +15,26 @@ def test_single_sign_on():
     token = SSOToken
     service.handle(Request("Emily"), token)
     spy_sso_registry.is_valid.assert_called_with(token)
+
+def test_single_sign_on_with_invalid_token():
+    spy_sso_registry = Mock(SingleSignOnRegistry)
+    spy_sso_registry.is_valid.return_value = False
+    service = MyService(spy_sso_registry)
+    token = SSOToken()
+    response = service.handle(Request("Emily"), token)
+    spy_sso_registry.is_valid.assert_called_with(token)
+    assert response.text == "Please sign in"
+
+def confirm_token(correct_token):
+    def is_valid(actual_token):
+        if actual_token != correct_token:
+            raise ValueError("wrong token received")
+    return is_valid
+
+def test_single_sign_on_receives_correct_token():
+    mock_sso_registry = Mock(SingleSignOnRegistry)
+    correct_token = SSOToken()
+    mock_sso_registry.is_valid = Mock(side_effect=confirm_token(correct_token))
+    service = MyService(mock_sso_registry)
+    service.handle(Request("Emily"), correct_token)
+    mock_sso_registry.is_valid.assert_called()
